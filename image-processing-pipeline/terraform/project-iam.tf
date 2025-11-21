79c9561f3e7b64f0360f129a008cd08c6c178126
Mark de Haan <mdh@e-dialog.group>
2025-11-21 07:15:23 +0100

    Add roles to processing pipeline SA

--- image-processing-pipeline/terraform/project-iam.tf
+++ image-processing-pipeline/terraform/project-iam.tf
@@ -1,6 +1,6 @@
 resource "google_service_account" "image_processing_pipeline" {
-    account_id = "image-processing-pipeline-sa"
-    display_name = "Image Processing Pipeline Service Account"
+  account_id   = "image-processing-pipeline-sa"
+  display_name = "Image Processing Pipeline Service Account"
 }
 
 resource "google_cloud_run_service_iam_member" "cloudtasks_video_generator_invoker" {
@@ -14,4 +14,46 @@ resource "google_pubsub_topic_iam_binding" "gcs_service_agent_publisher_role" {
   topic   = google_pubsub_topic.new_asset_notification_topic.id
   role    = "roles/pubsub.publisher"
   members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
+}
+
+resource "google_project_iam_member" "image_processing_pipeline_impersonate_task_sa" {
+  project = local.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = google_service_account.image_processing_pipeline.member
+}
+
+resource "google_project_iam_member" "image_processing_pipeline_storage_admin" {
+  project = local.project_id
+  role    = "roles/storage.admin"
+  member  = google_service_account.image_processing_pipeline.member
+}
+
+resource "google_project_iam_member" "image_processing_pipeline_bigquery_data_admin" {
+  project = local.project_id
+  role    = "roles/bigquery.admin"
+  member  = google_service_account.image_processing_pipeline.member
+}
+
+resource "google_project_iam_member" "image_processing_pipeline_cloud_tasks_enqueuer" {
+  project = local.project_id
+  role    = "roles/cloudtasks.enqueuer"
+  member  = google_service_account.image_processing_pipeline.member
+}
+
+resource "google_project_iam_member" "image_processing_pipeline_cloud_functions_invoker" {
+  project = local.project_id
+  role    = "roles/cloudfunctions.invoker"
+  member  = google_service_account.image_processing_pipeline.member
+}
+
+resource "google_project_iam_member" "image_processing_pipeline_cloud_run_invoker" {
+  project = local.project_id
+  role    = "roles/run.invoker"
+  member  = google_service_account.image_processing_pipeline.member
+}
+
+resource "google_project_iam_member" "image_processing_pipeline_vertex_ai_user" {
+  project = local.project_id
+  role    = "roles/aiplatform.user"
+  member  = google_service_account.image_processing_pipeline.member
 }
